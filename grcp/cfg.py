921175937d47bb9db23809e250eaf9e3573440be
Trung Truong <trungdtbk@gmail.com>
2018-10-16 03:06:32 +1300

    change default port to 9999

--- grcp/cfg.py
+++ grcp/cfg.py
@@ -3,7 +3,7 @@ from oslo_config import types
 
 opts = [
         cfg.StrOpt('bind_host', default='0.0.0.0', help='IP address to listen on.'),
-        cfg.Opt('bind_port', type=types.Integer(1024, 65535), default=9090,
+        cfg.Opt('bind_port', type=types.Integer(1024, 65535), default=9999,
             help='Port number to listen on.'),
         cfg.ListOpt('app_list', default=[], help='app module names to run.'),
         cfg.MultiStrOpt('app', default=[], positional=True, help='app module names to run.'),
