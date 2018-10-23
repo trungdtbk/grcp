60a06db76cf131fde28a48016f6bf6f15991bcd9
Trung Truong <trungdtbk@gmail.com>
2018-10-23 16:40:35 +1300

    start topology by default

--- grcp/grcp.py
+++ grcp/grcp.py
@@ -17,7 +17,8 @@ logger = get_logger('grcp', loglevel='debug')
 def main():
     logger.info('starting the controller')
     CONF(sys.argv[1:])
-    app_list = CONF.app_list + CONF.app
+    app_list = ['grcp.core.topology']
+    app_list = app_list + CONF.app_list + CONF.app
     app_manager = AppManager.get_instance()
     app_manager.load_apps(app_list)
     threads = app_manager.instantiate_apps()
