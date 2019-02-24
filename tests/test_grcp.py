5c5c4ea1220681fd7597f6171a8e6584ac9d9452
Trung Truong <trungdtbk@gmail.com>
2019-02-25 03:32:23 +1300

    test grcp.py

---
+++ tests/test_grcp.py
@@ -0,0 +1,34 @@
+import os
+import json
+import tempfile
+import unittest
+import subprocess
+import socket
+
+class GRcpServerTestBase(unittest.TestCase):
+
+    def setUp(self):
+        self.start_grcp()
+
+    def tearDown(self):
+        pass
+
+    def start_grcp(self, port=4567):
+        print(os.getcwd())
+        p = subprocess.Popen(['python3', '-m', 'grcp.grcp', '--bind_port', str(port)],
+                             stderr=subprocess.PIPE)
+        # test if grcp has started successfully
+        try:
+            (stdout_data, stderr_data) = p.communicate(timeout=10)
+            self.assertFalse(stderr_data)
+        except:
+            pass
+
+    def test_start_stop(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(10.0)
+        s.connect(('localhost', 4567))
+        msg = {'msg_type': 'router_up', 'routerid': '1.1.1.1'}
+        s.send(json.dumps(msg).encode('utf-8'))
+        s.close()
+        self.assertTrue(True)
